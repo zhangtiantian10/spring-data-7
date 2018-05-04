1273e7f454b3080bc886c71f809163bb518eb53a
Tian Zhang <975571814@qq.com>
2018-05-04 14:31:58 +0800

    write company entry
    Signed-off-by: Tian Zhang <975571814@qq.com>

--- src/main/java/com/example/employee/entity/Company.java
+++ src/main/java/com/example/employee/entity/Company.java
@@ -1,5 +1,39 @@
 package com.example.employee.entity;
 
-public class Company {
+import javax.persistence.*;
 
+@Entity
+@Table(name = "Company")
+public class Company {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Integer id;
+
+  private String companyName;
+
+  private Integer employeesNumber;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getCompanyName() {
+    return companyName;
+  }
+
+  public void setCompanyName(String companyName) {
+    this.companyName = companyName;
+  }
+
+  public Integer getEmployeesNumber() {
+    return employeesNumber;
+  }
+
+  public void setEmployeesNumber(Integer employeesNumber) {
+    this.employeesNumber = employeesNumber;
+  }
 }
